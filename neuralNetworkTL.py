f82874b87bad2b8a1c2f417358664eedd91a5432
stanML <benstanley@live.co.uk>
2016-01-06 17:45:38 +0000

    Updated Neural Network Class

--- neuralNetworkTL.py
+++ neuralNetworkTL.py
@@ -10,25 +10,54 @@ benstanley@live.co.uk
 
 # TODO: allow for more than one layer as a network parameter
 #       (use a generator??)
-# TODO: remove methods such as sigmoid, put in base clase that nn extends
-# TODO: Include set_parameter method and an assertion for data size compatability
-
-
+# TODO: Include public fit/predict method and an assertion for
+#       data size/weights compatability
+# TODO: public method getParameters
 
 import numpy as np
-from scipy import optimize
+from scipy.optimize import fmin_cg
 import pdb
 
 class neuralNetwork(object):
     
     # Initialize neural network parameters
     # and random weights (theta1 & theta2)
-    def __init__(self, input_layer_size, hidden_layer_size, num_labels, lamda):
+    def __init__(self, input_layer_size, num_labels,
+                 hidden_layer_size=None, lamda=None,
+                 add_bias=False):
+        
+        """
+        Use default network parameters
+        if none are specified
+        """
+        
+        if hidden_layer_size is None:
+            self.hl_size = 25
+            print "Warning used default parameter: 'hidden_layer_size'"
 
+        else:
+            self.hl_size = hidden_layer_size
+
+        if lamda is None:
+            self.lamda = 0.1
+            print "Warning used default parameter: 'lamda'"
+
+        else:
+            self.lamda = lamda
+
+        # Parse bias initial parameter
+        if add_bias == False:
+            init_bias = 0
+
+        elif add_bias == True:
+            init_bias = 1
+
+        else:
+            raise ValueError("Only boolean values can be used for 'add_bias'")
+    
         self.il_size = input_layer_size
-        self.hl_size = hidden_layer_size
         self.n_labels = num_labels
-        self.lamda = lamda
+        self.bias = 1
         
         """
         Initialises weights as a uniform 
@@ -38,16 +67,58 @@ class neuralNetwork(object):
         bias terms added to the input data.
         """
         
-        self.theta1 = np.random.uniform((-1/np.sqrt(self.il_size)),
+        theta1 = np.random.uniform((-1/np.sqrt(self.il_size)),
                                         (1/np.sqrt(self.il_size)),
-                                        [self.hl_size, self.il_size + 1])
+                                        [self.hl_size, self.il_size + init_bias])
         
-        self.theta2 = np.random.uniform((-1/np.sqrt(self.hl_size)),
+        theta2 = np.random.uniform((-1/np.sqrt(self.hl_size)),
                                         (1/np.sqrt(self.hl_size)),
-                                        [self.n_labels, self.hl_size + 1])
-   
+                                        [self.n_labels, self.hl_size + init_bias])
+            
+        self.nn_params = np.concatenate([theta1.ravel(),
+                                         theta2.ravel()])
+            
+    # Set weights to specified values
+    def setWeights(self, new_theta1, new_theta2, add_bias=False):
+    
+        """
+        Public method to set global parameter 
+        values to user specified values
+        """
+        # unroll parameters into theta1 & theta 2
+        theta1 = np.reshape(self.nn_params[0:self.hl_size * (self.il_size + self.bias)],
+                            [self.hl_size, (self.il_size + self.bias)])
+        theta2 = np.reshape(self.nn_params[(self.hl_size * (self.il_size + self.bias))::],
+                            [self.n_labels, (self.hl_size + self.bias)])
+        
+        print "Previous Theta1 parameter size = ", theta1.shape
+        print "Previous Theta1 parameter size = ", theta2.shape
+        print "------------------------------------"
+        
+        # Parse bias parameter
+        if add_bias == False:
+            pass
+        
+        elif add_bias == True:
+            np.insert(new_theta1, 0, 1, axis=1)
+            np.insert(new_theta2, 0, 1, axis=1)
+        
+        
+        else:
+            raise ValueError("Only boolean values can be used for 'add_bias'")
+        
+        # Set new parameter values
+        theta1 = new_theta1
+        theta2 = new_theta2
+        
+        print "New Theta1 parameter size = ", theta1.shape
+        print "New Theta1 parameter size = ", theta2.shape
+
+        self.nn_params = np.concatenate([theta1.ravel(),
+                                        theta2.ravel()])
+    
     # 'One-Hot' Matrix
-    def oneHot(self, y):
+    def __oneHot(self, y):
         
         y_ohm = np.zeros([len(y), self.n_labels])
         i = len(y) - 1
@@ -60,16 +131,8 @@ class neuralNetwork(object):
     
         return y_ohm
     
-    # Add biases
-    def addBias(self, array):
-    
-        ones  = np.ones((len(array),1))
-        array = np.concatenate((ones,array), axis=1)
-    
-        return array
-    
     # Sigmoid Activation function
-    def sigmoid(self, a):
+    def __sigmoid(self, a):
     
         """
         Takes as input a scalar value 
@@ -80,7 +143,7 @@ class neuralNetwork(object):
         return 1 / (1 + np.exp(-a))
     
     # Calculate the sigmoid gradient
-    def sigmoidGradient(self, z):
+    def __sigmoidGradient(self, z):
 
         """
         For each value of z (activations 
@@ -88,12 +151,12 @@ class neuralNetwork(object):
         the gradient. Takes as input a 
         scalar value or array of values.
         """
-        gz = self.sigmoid(z)
+        gz = self.__sigmoid(z)
         
         return np.multiply(gz, (1 - gz))
 
     # Perform Fowarad Propagation
-    def forwardProp(self,X):
+    def __forwardProp(self,X, theta1, theta2):
     
         """
         A vectorized implementation of 
@@ -104,26 +167,26 @@ class neuralNetwork(object):
         """
         
         # Add a column of ones to X
-        a1 = self.addBias(X)
+        a1 = np.insert(X, 0, 1, axis=1)
         
         # Apply the weights and sum each column for the
         # activation of each unit in the hidden layer
-        z2 = np.dot(a1, np.transpose(self.theta1))
+        z2 = np.dot(a1, np.transpose(theta1))
 
         # Apply the sigmoid function to the activations
         # in the hidden layer
-        a2 = self.sigmoid(z2)
+        a2 = self.__sigmoid(z2)
         
         # Add bias term to hidden layer activations
         a2 = np.insert(a2, 0, 1, axis=1)
         
         # Apply the weights and sum each column for the
         # activation of each unit in the ouput layer
-        z3 = np.dot(a2, np.transpose(self.theta2))
+        z3 = np.dot(a2, np.transpose(theta2))
 
         # Apply the sigmoid function to the activations in
         # the ouput layer
-        hyp = self.sigmoid(z3)
+        hyp = self.__sigmoid(z3)
 
         return hyp, a1, a2, z2
 
@@ -138,21 +201,28 @@ class neuralNetwork(object):
         -fitting, this is weighted with lamda.
         """
         
+        # Unroll parameter vector into two matrices
+        theta1 = np.reshape(self.nn_params[0:self.hl_size * (self.il_size + self.bias)],
+                                [self.hl_size, (self.il_size + self.bias)])
+                                
+        theta2 = np.reshape(self.nn_params[(self.hl_size * (self.il_size + self.bias))::],
+                                [self.n_labels, (self.hl_size + self.bias)])
+        
         # Convert target values to one hot matrix
-        y_matrix = self.oneHot(y)
+        y_matrix = self.__oneHot(y)
         
         # Scaling term for regularization
         m = float(len(X))
 
-        hyp, a1, a2, z2 = self.forwardProp(X)
+        hyp, a1, a2, z2 = self.__forwardProp(X, theta1, theta2)
         
         # Cross-entropy cost function
         J = (1/m) * (np.sum(y_matrix * np.log(hyp))) - (np.sum((1 - y_matrix) *
                                                               np.log(1 - hyp)))
     
         # Regularisation term
-        reg_term = (self.lamda/(2*m)) * np.sum(np.square(self.theta1[:,1::])) \
-                    + np.sum(np.square(self.theta2[:,1::]))
+        reg_term = (self.lamda/(2*m)) * np.sum(np.square(theta1[:,1::])) \
+                    + np.sum(np.square(theta2[:,1::]))
         
         # Regularised cost function
         J += reg_term
@@ -169,7 +239,8 @@ class neuralNetwork(object):
         d_3 = hyp - y_matrix
         
         # Compute the error at the activations of the hidden layer
-        d_2 = np.multiply(np.dot(d_3, self.theta2[:, 1::]), self.sigmoidGradient(z2))
+        d_2 = np.multiply(np.dot(d_3, theta2[:, 1::]),
+                          self.__sigmoidGradient(z2))
         
         # Compute the error at each layer
         delta_1 = np.dot(np.transpose(d_2), a1)
@@ -180,8 +251,8 @@ class neuralNetwork(object):
         theta2_grad = delta_2 / m
         
         # Create a local copy of theta1 & theta2
-        theta1_reg = self.theta1
-        theta2_reg = self.theta2
+        theta1_reg = theta1
+        theta2_reg = theta2
         
         # Exclude the 1st column from the local
         # parameter values from the regularisation
@@ -196,13 +267,6 @@ class neuralNetwork(object):
                                    theta2_grad.ravel()])
 
         return J, nn_params
-
-
-
-
-
-
-
 
 
 
